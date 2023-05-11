2dcbc056cf1356d5ca93ce32372edf34fb274db8
manas <manasgupta733@gamil.com>
2023-05-11 20:59:50 +0530

    chat app DONE

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -2,9 +2,29 @@ import 'package:chat/widgets/chat_messages.dart';
 import 'package:chat/widgets/new_messages.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotification() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+    // final token = await fcm.getToken();
+    // print(token);
+    fcm.subscribeToTopic('chat');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotification();
+  }
 
   @override
   Widget build(BuildContext context) {
